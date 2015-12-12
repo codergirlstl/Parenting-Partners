753be6522a020e5c334fd5ab7d35607c9989d737
CoderGirlSTL <Sheila@Sheilas-MacBook-Air.local>
2015-12-12 11:17:24 -0600

    add email

--- contacts/address_book.rb
+++ contacts/address_book.rb
@@ -20,6 +20,7 @@ class AddressBook
       puts contact.to_s('full_name')
       contact.print_phone_numbers
       contact.print_addresses
+      contact.print_emails
       puts "\n"
     end
   end
@@ -37,6 +38,7 @@ address_book = AddressBook.new
 parent = Contact.new
 parent.first_name = "Mama"
 parent.last_name = "Odie"
+parent.add_email("mamaodie@internet.com")
 parent.add_phone_number("Home", "123-456-7890")
 parent.add_phone_number("Work", "456-789-0123")
 parent.add_address("Home", "123 Main St.", "", "Saint Louis", "MO", "12345")
@@ -44,6 +46,7 @@ parent.add_address("Home", "123 Main St.", "", "Saint Louis", "MO", "12345")
 babysitter = Contact.new
 babysitter.first_name = "Mary"
 babysitter.last_name = "Poppins"
+babysitter.add_email("marypoppins@internet.com")
 babysitter.add_phone_number("Home", "222-222-2222")
 babysitter.add_address("Home", "222 Two Lane", "", "St. Louis", "MO", "12345")
 
