bd68f07c064f403f83579c33d95b9162c1d7226e
CoderGirlSTL <Sheila@Sheilas-Air.att.net>
2016-01-12 19:08:26 -0600

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
     get "/pages/:page" => "pages#show"
     root "pages#show", page: "home"
-    resources :todo_lists
+    resources :todo_lists, :calendar
 end
 
-CalendarExample::Application.routes.draw do
-    resource :calendar, only: [:show], controller: :calendar
-    root to: "calendar#show"
-end
+#CalendarExample::Application.routes.draw do
+#resource :calendar, only: [:show], controller: :calendar
+#root to: "calendar#show"
+#end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216210625) do
+ActiveRecord::Schema.define(version: 20160112185609) do
+
+  create_table "conversations", force: :cascade do |t|
+  end
+
+  create_table "messages", force: :cascade do |t|
+  end
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
