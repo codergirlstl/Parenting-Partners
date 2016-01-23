3a0451701b39c9f6b511195e1d0ded3444d41fc2
CoderGirlSTL <Sheila@Sheilas-Air.att.net>
2016-01-23 16:23:21 -0600

    adding missing code

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-
-  get 'user_sessions/create'
-
     get "/pages/:page" => "pages#show"
     root "pages#show", page: "home"
-    resources :user
+    resources :users
+    resources :user_sessions, only: [:new, :create]
     resources :todo_lists do
         resources :todo_items do
             member do
@@ -13,20 +10,14 @@ Rails.application.routes.draw do
             end
         end
     end
-    'todo_lists#index'
+    get 'todo_item'=>'todo_lists#index'
     resources :conversations do
         resources :messages
         end
     get 'about' =>'about_pages#about'
     get 'faq' =>'faq_pages#faq'
+    get 'login' =>'user_session#new'
     end
 
-#CalendarExample::Application.routes.draw do
-  get 'user_sessions/new'
 
-  get 'user_sessions/create'
-
-#resources :calendar, only: [:show], controller: :calendar
-#root to: "calendar#show"
-#end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118174845) do
+ActiveRecord::Schema.define(version: 20160113015246) do
 
   create_table "calendars", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -35,16 +35,5 @@ ActiveRecord::Schema.define(version: 20160118174845) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email"
 
 end
