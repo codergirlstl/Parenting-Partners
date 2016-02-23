34dcaeefca8e2e1eda779bc7ca44928919634de9
CoderGirlSTL <Sheila@Sheilas-Air.att.net>
2016-02-22 19:31:20 -0600

    Remove bullets etc

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -14,10 +14,10 @@ class TodoItemsController < ApplicationController
     def create
         @todo_item = @todo_list.todo_items.new(todo_item_params)
         if @todo_item.save
-            flash[:success] = "Added todo list item."
+            flash[:success] = "Added request list item."
             redirect_to todo_list_todo_items_path
             else
-            flash[:error] = "There was a problem adding that todo list item."
+            flash[:error] = "There was a problem adding that request list item."
             render action: :new
         end
     end
@@ -29,10 +29,10 @@ class TodoItemsController < ApplicationController
     def update
         @todo_item = @todo_list.todo_items.find(params[:id])
         if @todo_item.update_attributes(todo_item_params)
-            flash[:success] = "Saved todo list item."
+            flash[:success] = "Saved requst list item."
             redirect_to todo_list_todo_items_path
             else
-            flash[:error] = "That todo item could not be saved."
+            flash[:error] = "That request item could not be saved."
             render action: :edit
         end
     end
@@ -40,9 +40,9 @@ class TodoItemsController < ApplicationController
     def destroy
         @todo_item = @todo_list.todo_items.find(params[:id])
         if @todo_item.destroy
-            flash[:success] = "Todo list item was deleted."
+            flash[:success] = "Request list item was deleted."
             else
-            flash[:error] = "Todo list item could not be deleted."
+            flash[:error] = "Request list item could not be deleted."
         end
         redirect_to todo_list_todo_items_path
     end
@@ -50,7 +50,7 @@ class TodoItemsController < ApplicationController
     def complete
         @todo_item = @todo_list.todo_items.find(params[:id])
         @todo_item.toggle_completion!
-        redirect_to todo_list_todo_items_path, notice: "Todo item updated."
+        redirect_to todo_list_todo_items_path, notice: "Request item updated."
     end
     
     def url_options
